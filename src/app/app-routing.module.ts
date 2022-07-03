7eae9c28298899d4011014b0c442826a6115935b
BoubaM2i <boubacar.dembele@labom2iformation.fr>
2022-07-03 19:45:57 +0200

    TPAngular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component'; 
+import { NotFoundComponent } from './_pages/not-found/not-found.component'; 
 
-const routes: Routes = [];
+const routes : Routes = [
+  // Déclaration de la correspondance entre URI et le composant
+  // Le terme 
+  {path : '', redirectTo : 'home', pathMatch : 'full'},
+  {path : 'home', component : HomePageComponent},
+  {path : 'products', component : ProductsPageComponent},
+  {path : '**', component : NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_commons/navbar/navbar.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { HomePageComponent } from './_pages/home-page/home-page.component';
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
+import {MatSliderModule} from '@angular/material/slider'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './products-page/product-list/product-list.component';
+import { ProductCardComponent } from './products-page/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductsPageComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
